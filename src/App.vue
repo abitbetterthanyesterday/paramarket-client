76a8a8faaa6f955db47fe59ea79804ac36d37984
Aloys Berger <aloysberger@gmail.com>
2020-08-03 14:36:10 +0200

    Add type js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <h1>App</h1>
+    <h1>Paramarket client</h1>
     <router-view></router-view>
   </v-app>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,53 @@
 <template>
-  <div class="home">
-    <h1>Home sweet home!</h1>
-  </div>
-</template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <div id="typed-strings">
+  <p>Typed.js is a <strong>JavaScript</strong> library.</p>
+  <p>It <em>types</em> out sentences.</p>
+</div>
+<span id="typed"></span>
+        <v-col cols="12">
+          <v-text-field
+            v-model="message"
+            outlined
+            clearable
+            :label=this.searchAnimation
+            type="text"
+          >
+            <template v-slot:append>
+              <v-fade-transition leave-absolute>
+                <v-progress-circular
+                  v-if="loading"
+                  size="24"
+                  color="info"
+                  indeterminate
+                ></v-progress-circular>
+                <img v-else width="24" height="24" src="https://cdn.vuetifyjs.com/images/logos/v-alt.svg" alt="">
+              </v-fade-transition>
+            </template>
+          </v-text-field>
+        </v-col>
 
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
 <script>
 // @ is an alias to /src
 
 export default {
   name: 'Home',
   components: {
+  },
+  mounted(){
+
+  },
+  data(){
+    return{
+      searchAnimation: 'here'
+    }
   }
 }
 </script>
